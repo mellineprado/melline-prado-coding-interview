9d0f0f45e0b4a8fd17747f86be7cdc485e4b83c8
melline prado <mellinevprado@hotmail.com>
2026-02-04 14:06:39 -0300

    feat: add by_username scope to filter tweets by username

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   def index
     tweets = Tweet.order(created_at: :desc)
 
-    tweets = tweets.by_user_id(params[:user_id]) if params[:user_id].present?
+    tweets = tweets.by_username(params[:username]) if params[:username].present?
 
     page = (params[:page] || 1).to_i
     per_page = (params[:per_page] || 5).to_i
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,5 @@ class Tweet < ApplicationRecord
   belongs_to :user
 
   scope :by_user_id, -> (user_id) { where(user_id: user_id) if user_id.present? }
+  scope :by_username, -> (username) { joins(:user).where('users.username LIKE ?', username) if username.present? }
 end
