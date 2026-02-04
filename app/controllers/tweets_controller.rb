8321a9c65388441e09436a676020b3aaf4718a0d
melline prado <mellinevprado@hotmail.com>
2026-02-04 13:53:11 -0300

    feat: add pagination to tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,13 @@
 class TweetsController < ApplicationController
   def index
-    tweets = Tweet.all
+    tweets = Tweet.order(created_at: :desc)
 
     tweets = tweets.by_user_id(params[:user_id]) if params[:user_id].present?
+
+    page = (params[:page] || 1).to_i
+    per_page = (params[:per_page] || 5).to_i
+
+    tweets = tweets.limit(per_page).offset((page - 1) * per_page)
     render json: tweets
   end
 end
